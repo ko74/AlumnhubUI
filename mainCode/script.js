7b1948d11bed154f1e2ac27b9693c55cf787eb59
ko74 <francisadjei801@gmail.com>
2021-03-21 15:39:04 +0000

    initial code for the UIs

---
+++ mainCode/script.js
@@ -0,0 +1,7 @@
+// import "mainCode\adminPage\AdminPanel.html";
+// import "mainCode\RegPage\index.html"
+
+document.getElementById("subBtn").addEventListener("click", adminPage);
+    function adminPage(){
+        window.location.href="mainCode\adminPage\AdminPanel.html"
+}
